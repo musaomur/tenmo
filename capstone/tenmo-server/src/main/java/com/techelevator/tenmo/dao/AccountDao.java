22972e5d7ce6d3c027fa0124780ab0f38c246bbf
Nathanial White <nawhite1505@gmail.com>
2024-03-06 16:30:44 -0500

    .

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -3,7 +3,8 @@ package com.techelevator.tenmo.dao;
 import com.techelevator.tenmo.model.Account;
 
 public interface AccountDao {
-Account getBalance();
-Account getBalanceByUserId(int id);
+    Account getBalance();
+    Account getBalanceByUserId(int id);
+
 
 }
