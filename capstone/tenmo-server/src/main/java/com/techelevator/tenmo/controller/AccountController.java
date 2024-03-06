bf2b47314cb6066357056bee49d50dd2bca7036c
Bill Greenrod <billifordgrod@gmail.com>
2024-03-06 13:21:26 -0500

    this is a test

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,21 +1,8 @@
 package com.techelevator.tenmo.controller;
 
-import com.techelevator.tenmo.dao.AccountDao;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 import com.techelevator.tenmo.security.jwt.TokenProvider;
 
 public class AccountController {
-
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-    private final AccountDao accountDao;
-
-    public AccountController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder, AccountDao accountDao) {
-        this.tokenProvider = tokenProvider;
-        this.authenticationManagerBuilder = authenticationManagerBuilder;
-        this.accountDao = accountDao;
-    }
-
-
 }
